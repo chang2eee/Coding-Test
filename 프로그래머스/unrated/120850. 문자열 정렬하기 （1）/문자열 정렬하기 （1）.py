b141abe2a3f2780b249530ed3308e2fa7ea27833
김병창 <106654981+chang2eee@users.noreply.github.com>
2023-11-26 20:20:45 +0900

    [level unrated] Title: 문자열 정렬하기 (1), Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer = []
+    
+    for string in my_string:
+        if string.isdigit():
+            answer.append(int(string))
+            
+    answer.sort()
+    
+    return answer
