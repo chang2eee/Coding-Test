db647c590d9f9b87af496f89e021721c45904f47
김병창 <106654981+chang2eee@users.noreply.github.com>
2023-11-26 14:58:16 +0900

    [level unrated] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -0,0 +1,11 @@
+def solution(my_string, is_suffix):
+    answer = 0
+    temp = []
+    
+    for i in range(len(my_string)):
+        temp.append(my_string[i:])
+    
+    if is_suffix in temp:
+        answer = 1
+    
+    return answer
