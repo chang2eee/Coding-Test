7bb8bc9d024122d8eea32cd7a7b9c918639b8cd5
김병창 <106654981+chang2eee@users.noreply.github.com>
2023-11-29 19:06:56 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,20 @@
+def solution(num):
+    answer = 0
+    
+    if num == 1:
+        answer = 0
+        return answer
+    
+    while num != 1 and answer < 500:
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num = num * 3 + 1
+        answer += 1
+    
+    if answer >= 500:
+        answer = -1
+        
+    
+    
+    return answer
