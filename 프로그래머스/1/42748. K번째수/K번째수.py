2dcbc1c878025c44e3dd05ea71758590997989f1
김병창 <106654981+chang2eee@users.noreply.github.com>
2023-11-30 16:32:29 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,12 @@
+def solution(array, commands):
+    answer = []
+    
+    for command in commands:
+        i, j , k = command
+        
+        temp = array[i-1:j]
+        temp.sort()
+        
+        answer.append(temp[k-1])
+    
+    return answer
