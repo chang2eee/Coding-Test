30bfa6d699989da8aebdc0f74b41a2bd066989a3
김병창 <106654981+chang2eee@users.noreply.github.com>
2023-12-15 00:50:12 +0900

    [level 1] Title: 덧칠하기, Time: 1.13 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,12 +1,3 @@
 def solution(n, m, section):
     answer = 0
-    start = 0
-    
-    for sect in section:
-        if sect >= start:
-            start = sect + m
-            answer += 1
-        else:
-            continue
-    
     return answer
