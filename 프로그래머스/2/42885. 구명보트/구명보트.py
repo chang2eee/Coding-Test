34dfff3e46c98929e61d515ca309237b0c7dec20
김병창 <106654981+chang2eee@users.noreply.github.com>
2023-12-20 13:30:01 +0900

    [level 2] Title: 구명보트, Time: 8.52 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,14 @@
+def solution(people, limit):
+    answer = 0
+
+    start, end = 0, len(people) - 1
+
+    people.sort()
+
+    while people and start <= end:
+        if people[start] + people[end] <= limit:
+            start += 1
+        end -= 1
+        answer += 1
+
+    return answer
