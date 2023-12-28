d8c70cb21176b638c022bcb081d455c5237f1954
김병창 <106654981+chang2eee@users.noreply.github.com>
2023-12-28 22:02:28 +0900

    [level 2] Title: 전화번호 목록, Time: 443.27 ms, Memory: 34.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+def solution(phone_book):
+    answer = True
+    
+    phone_book.sort()
+    
+    phone_book_counter = Counter(phone_book)    
+    
+    for phone_number in phone_book:
+        temp = ''
+        for number in phone_number:
+            temp += number
+            
+            if temp in phone_book_counter and temp != phone_number:
+                answer = False
+                return answer
+    
+    return answer
