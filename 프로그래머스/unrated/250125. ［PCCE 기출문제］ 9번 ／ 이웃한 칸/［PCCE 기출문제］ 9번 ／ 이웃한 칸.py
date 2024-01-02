14cdd52010a2aee74b1e1ec696e3cb577ccb9bda
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-01-02 17:17:10 +0900

    [level unrated] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
+++ 프로그래머스/unrated/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
@@ -1,17 +1,18 @@
 def solution(board, h, w):
     answer = 0
-    height = len(board)
-    width = len(board[0])
-    
     target = board[h][w]
+    height, width = len(board), len(board[0])
+    
+    if check(h-1, w, height, width) and board[h-1][w] == target:
+        answer += 1
+    
+    if check(h+1, w, height, width) and board[h+1][w] == target:
+        answer += 1
     
     if check(h, w-1, height, width) and board[h][w-1] == target:
         answer += 1
+    
     if check(h, w+1, height, width) and board[h][w+1] == target:
-        answer += 1
-    if check(h-1, w, height, width) and board[h-1][w] == target:
-        answer += 1
-    if check(h+1, w, height, width) and board[h+1][w] == target:
         answer += 1
     
     return answer
