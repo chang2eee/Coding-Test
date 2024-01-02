b109f27fbb3c7fd4c1cfc5ff42f5d5a3e9ba73c1
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-01-03 00:44:16 +0900

    [level 2] Title: 방문 길이, Time: 0.23 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -0,0 +1,23 @@
+def solution(dirs):
+    answer = 0
+    
+    command = {'U': (0, 1), 'D': (0, -1), 'L': (-1, 0), 'R': (1, 0)}
+    
+    start_x, start_y = 0, 0
+    visited = set()
+    
+    for element in dirs:
+        next_x = start_x + command[element][0]
+        next_y = start_y + command[element][1]
+        
+        route = (start_x, start_y, next_x, next_y)
+        
+        if  -5 <= next_x <= 5 and -5 <= next_y <= 5: 
+            visited.add(route)
+            visited.add((next_x, next_y, start_x, start_y)) 
+            start_x, start_y = next_x, next_y
+    
+    answer = len(visited) // 2  
+    
+    
+    return answer
