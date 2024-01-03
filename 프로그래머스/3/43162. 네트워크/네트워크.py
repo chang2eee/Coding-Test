26d48d14b11a056da276ec42cdfff6b8597a00ba
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-01-03 14:49:44 +0900

    [level 3] Title: 네트워크, Time: 0.81 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,29 @@
+def solution(n, computers):
+    answer = 0
+    
+    graph = dict()
+    
+    for idx in range(len(computers)):
+        graph[str(idx)] = []
+        for i in range(len(computers[idx])):
+            if idx != i and computers[idx][i] == 1:
+                graph[str(idx)].append(str(i))
+    
+    visited = [False] * len(computers)
+    
+    for node in graph:
+        if not visited[int(node)]:
+            dfs(graph, int(node), visited)
+            answer += 1
+    
+    return answer
+
+def dfs(graph, start, visited):
+    visited[start] = True
+    
+    for neighbor in graph[str(start)]:
+        if visited[int(neighbor)] == False:
+            dfs(graph, int(neighbor), visited)
+    
+    
+    
