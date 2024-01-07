d9ada178335fd9cbd4bb369dc84b299aee280175
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-01-07 16:15:03 +0900

    [level 2] Title: 롤케이크 자르기, Time: 619.53 ms, Memory: 51.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+def solution(topping):
+    answer = 0
+    
+    topping_counter = Counter(topping)
+    left = set()
+    
+    for element in topping:
+        topping_counter[element] -= 1
+        left.add(element)
+        
+        if topping_counter[element] == 0:
+            topping_counter.pop(element)
+        
+        if len(topping_counter) == len(left):
+            answer += 1
+    
+    return answer
