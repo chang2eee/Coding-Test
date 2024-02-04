0fbd5873aba20eb537df9aa974c15fe8c9f688e3
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-02-04 19:27:49 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -3,30 +3,29 @@ from math import ceil
 def solution(progresses, speeds):
     answer = []
     
-    remain = []
+    progresses = [100 - progress for progress in progresses]
+    days = []
     
-    for progress in progresses:
-        remain.append(100 - progress)
+    for progress, speed in zip(progresses, speeds):
+        days.append(ceil(progress / speed))
     
     temp = []
     
-    for r, s in zip(remain, speeds):
-        temp.append(ceil(r / s))
-    
-    stack = []
-    
-    for element in temp:
-        if not stack:
-            stack.append(element)
+    for day in days:
+        if not temp:
+            temp.append(day)
         else:
-            if max(stack) >= element:
-                stack.append(element)
+            if max(temp) >= day:
+                temp.append(day)
             else:
-                answer.append(len(stack))
-                stack.clear()
-                stack.append(element)
-    
-    if stack:
-        answer.append(len(stack))
-    
+                if max(temp) >= day:
+                    temp.append(day)
+                else:
+                    answer.append(len(temp))
+                    temp.clear()
+                    temp.append(day)
+            
+    if temp:
+        answer.append(len(temp))
+                    
     return answer
