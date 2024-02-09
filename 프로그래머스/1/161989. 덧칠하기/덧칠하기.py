16472acde054811ffb34637337ebf99034bd27ed
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-02-09 22:30:45 +0900

    [level 1] Title: 덧칠하기, Time: 9.83 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,3 +1,17 @@
 def solution(n, m, section):
     answer = 0
+    
+    checked = [False] * n
+    section = [sec - 1 for sec in section]
+    
+    section.sort()
+    
+    for sec in section:
+        if not checked[sec]:
+            end_idx = min(sec + m, n)
+            
+            for idx in range(sec, end_idx):
+                checked[idx] = True
+            answer += 1
+    
     return answer
