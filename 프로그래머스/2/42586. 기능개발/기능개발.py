70d42e271d549bd9ed95a3d2891d419ad2ad9cac
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-02-09 21:30:28 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -4,28 +4,26 @@ def solution(progresses, speeds):
     answer = []
     
     progresses = [100 - progress for progress in progresses]
-    days = []
     
+    remain = []
     for progress, speed in zip(progresses, speeds):
-        days.append(ceil(progress / speed))
-    
+        remain.append(ceil(progress / speed))
+        
     temp = []
     
-    for day in days:
-        if not temp:
+    for day in remain:
+        if len(temp) == 0:
             temp.append(day)
         else:
             if max(temp) >= day:
                 temp.append(day)
             else:
-                if max(temp) >= day:
-                    temp.append(day)
-                else:
-                    answer.append(len(temp))
-                    temp.clear()
-                    temp.append(day)
-            
-    if temp:
+                answer.append(len(temp))
+                temp.clear()
+                temp.append(day)
+    
+    if len(temp) != 0:
         answer.append(len(temp))
-                    
+            
+    
     return answer
