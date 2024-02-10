9d50249633e6fe8e53e15cb5540a60f60379c46f
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-02-10 21:33:28 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,14 +1,18 @@
 def solution(k, score):
     answer = []
-    a=[]
     
-    for i in score:
-        if len(a)<k:
-            a.append(i)
+    temp = []
+    
+    for s in score:
+        if len(temp) < k:
+            temp.append(s)
         else:
-            if min(a)<i:
-                a.remove(min(a))
-                a.append(i)
-        answer.append(min(a))
+            if min(temp) < s:
+                temp.remove(min(temp))
+                temp.append(s)
+                
         
+        answer.append(min(temp))
+    
+    
     return answer
