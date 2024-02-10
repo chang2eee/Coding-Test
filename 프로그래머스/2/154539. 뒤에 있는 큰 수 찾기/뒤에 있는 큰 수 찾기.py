05d94c9f4185884352a8154982008335c7adfc11
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-02-10 17:11:09 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 155.42 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -1,27 +1,3 @@
-# from collections import deque
-
-# def solution(numbers):
-#     answer = [-1] * len(numbers)
-#     queue = deque(numbers)
-    
-#     idx = 0
-    
-#     while queue:
-#         number = queue.popleft()
-        
-#         if not queue:
-#             answer[idx] = -1
-#         else:
-#             for element in queue:
-#                 if number < element:
-#                     answer[idx] = element
-#                     break
-#                 else:
-#                     continue
-#         idx += 1
-    
-#     return answer
-
 def solution(numbers):
     answer = [-1] * len(numbers)
     stack = []
