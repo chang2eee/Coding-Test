4a0532b842ed684f0eb3bdcced2cd8a3d3bf2545
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-02-10 17:19:24 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,22 +1,27 @@
 from collections import deque
 
 def solution(begin, target, words):
+    answer = 0
+    
     n = len(words)
+    
     visited = [False] * n
     
-    queue = deque([(begin, 0)]) 
+    queue = deque([(begin, 0)])
     
     while queue:
         current, depth = queue.popleft()
+        
         if current == target:
             return depth
-
-        for i, word in enumerate(words):
-            if not visited[i] and can_convert(current, word):
-                visited[i] = True
-                queue.append((word, depth + 1))
-
-    return 0  
+        
+        for idx, word in enumerate(words):
+            if visited[idx] == False and can_convert(current, word):
+                visited[idx] = True
+                queue.append((word, depth+1))
+    
+    
+    return answer
 
 def can_convert(word1, word2):
     count = 0
@@ -26,4 +31,3 @@ def can_convert(word1, word2):
             count += 1
     
     return count == 1
- 
