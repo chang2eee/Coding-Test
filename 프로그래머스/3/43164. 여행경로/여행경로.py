2ccc500f6c88fd094d08a92ab4b7db7acc4c57b8
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-02-10 20:59:55 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,34 +1,28 @@
-from collections import deque
-
 def solution(tickets):
-    routes = dict()
-
-    for ticket in tickets:
-        departure, arrival = ticket
-        if departure not in routes:
-            routes[departure] = [arrival]
-        else:
-            routes[departure].append(arrival)
-            routes[departure].sort(reverse=True)
-
     answer = []
+    
+    visited = [False]*len(tickets)
+    
+    def dfs(airport, path):
+        if len(path) == len(tickets)+1:
+            answer.append(path)
+            return
+        
+        for idx, ticket in enumerate(tickets):
+            if airport == ticket[0] and visited[idx] == False:
+                visited[idx] = True
+                dfs(ticket[1], path+[ticket[1]])
+                visited[idx] = False
+        
+        
+    dfs("ICN", ["ICN"])
+    
+    answer.sort()
 
-    queue = deque(['ICN'])
-
-    while queue:
-        current_airport = queue[-1]
-
-        if current_airport in routes and routes[current_airport]:
-            next_airport = routes[current_airport].pop()
-            queue.append(next_airport)
-        else:
-            answer.append(queue.pop())
-
-    answer.reverse()
-
-    return answer
+    return answer[0]
 
 
+# from collections import deque
 
 # def solution(tickets):
 #     routes = dict()
@@ -43,68 +37,16 @@ def solution(tickets):
 
 #     answer = []
 
-#     stack = ['ICN']
+#     queue = deque(['ICN'])
 
-#     while stack:
-#         current_airport = stack[-1]
+#     while queue:
+#         current_airport = queue[0]
 
 #         if current_airport in routes and routes[current_airport]:
 #             next_airport = routes[current_airport].pop()
-#             stack.append(next_airport)
-#         else:
-#             answer.append(stack.pop())
+#             queue.append(next_airport)
+        
+#         answer.append(queue.popleft())
 
-#     answer.reverse()
 
 #     return answer
-
-
-
-# answer = []
-
-# def solution(tickets):
-#     global answer
-    
-#     start = 'ICN'
-    
-#     routes = dict()
-    
-#     for ticket in tickets:
-#         departure, arrival = ticket[0], ticket[1]
-#         if departure not in routes:
-#             routes[departure] = [arrival]
-#         else:
-#             routes[departure].append(arrival)
-#             routes[departure].sort()
-            
-#     visited = dict()
-    
-#     for key, value in routes.items():
-#         temp = []
-        
-#         for element in value:
-#             if element in routes:
-#                 temp.append(element)
-        
-#         routes[key] = temp
-        
-#         visited[key] = len(temp)
-    
-#     print(routes)
-#     print(visited)
-
-#     dfs(start, routes, visited, answer)
-    
-#     return answer
-
-# def dfs(start, routes, visited, answer):
-#     if start in visited and visited[start] > 0:
-#         visited[start] -= 1
-#         answer.append(start)
-#         start = routes[start].pop(0)
-        
-#         dfs(start, routes, visited, answer)
-#     else:
-#         answer.append(start)
-    
-#     return 
