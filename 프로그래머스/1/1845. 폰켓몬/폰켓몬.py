88d548f862fad618038aea881d6b29526270fd25
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-02-10 21:41:16 +0900

    [level 1] Title: 폰켓몬, Time: 0.83 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -5,11 +5,14 @@ def solution(nums):
     
     pokemon_counter = Counter(nums)
     take_amount = len(nums) // 2
+    
     pokemon_kind = len(pokemon_counter.keys())
     
     if pokemon_kind > take_amount:
         answer = take_amount
     else:
         answer = pokemon_kind
+    
+    
     
     return answer
