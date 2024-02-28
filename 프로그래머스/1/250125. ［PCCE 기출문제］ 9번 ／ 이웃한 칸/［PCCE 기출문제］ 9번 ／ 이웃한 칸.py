5a53692c91ac10caac6b4fce432b74669813c7fd
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-02-28 13:38:00 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
@@ -3,24 +3,22 @@ from collections import deque
 def solution(board, h, w):
     answer = 0
     
-    target = board[h][w]
-    
-    height, width = len(board), len(board[0])
-    
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     
-    queue = deque([(h, w)])
+    start_y, start_x = h, w
+    target = board[start_y][start_x]
+    
+    queue = deque([(start_y, start_x)])
     
     while queue:
         y, x = queue.popleft()
         
         for i in range(4):
-            next_x = x + dx[i]
             next_y = y + dy[i]
+            next_x = x + dx[i]
             
-            if 0 <= next_x < width and 0 <= next_y < height and board[next_y][next_x] == target:
+            if 0 <= next_y <= len(board) - 1 and 0 <= next_x <= len(board[0]) - 1 and board[next_y][next_x] == target:
                 answer += 1
-    
     
     return answer
