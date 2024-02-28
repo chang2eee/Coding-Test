2e8c1048f44d0d35d1663d770e854aa5d66d1f0f
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-02-28 17:18:26 +0900

    [level 2] Title: 리코쳇 로봇, Time: 2.27 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -2,46 +2,49 @@ from collections import deque
 
 def solution(board):
     answer = 0
-
-    start_x, start_y = 0, 0
-    
-    visited = set()
-    
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] == 'R':
-                start_x, start_y = j, i
-    
-    queue = deque([(start_x, start_y, 0)])
     
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     
+    start_y, start_x = 0, 0
+    
+    # 방문 좌표 기록
+    visited = set()
+    
+    # 로봇 위치 찾기
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 'R':
+                start_y, start_x = i, j
+    
+    # y, x, 거리
+    queue = deque([(start_y, start_x, 0)])
+    
     while queue:
-        x, y, distance = queue.popleft()
+        y, x, answer = queue.popleft()
         
-        if (x, y) in visited:
+        if (y, x) in visited:
             continue
-            
-        if board[y][x] == 'G':  
-            return distance
         
-        visited.add((x, y))
-        
+        if board[y][x] == 'G':
+            return answer
+    
+        visited.add((y, x))
+
         for i in range(4):
-            now_x = x
             now_y = y
+            now_x = x
             
             while True:
                 next_x = now_x + dx[i]
                 next_y = now_y + dy[i]
                 
-                if not (0 <= next_x < len(board[0]) and 0 <= next_y < len(board) and board[next_y][next_x] != 'D'):  
+                if not (0 <= next_y <= len(board) - 1 and 0 <= next_x <= len(board[0]) -1 and board[next_y][next_x] != 'D'):
                     break
-                
+                    
                 now_x = next_x
                 now_y = next_y
                 
-            queue.append((now_x, now_y, distance + 1))
+            queue.append((now_y, now_x, answer + 1))
     
     return -1
