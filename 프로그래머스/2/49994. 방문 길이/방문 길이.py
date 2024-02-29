c121f5cc1c9a066f2eaec17cd7d2f167fea99275
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-02-29 18:17:19 +0900

    [level 2] Title: 방문 길이, Time: 0.49 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,23 +1,24 @@
 def solution(dirs):
     answer = 0
     
-    command = {'U': (0, 1), 'D': (0, -1), 'L': (-1, 0), 'R': (1, 0)}
-    
+    directions = {'U': (0, 1), 'D': (0, -1), 'L': (-1, 0), 'R': (1, 0)}
     start_x, start_y = 0, 0
+    
     visited = set()
     
     for element in dirs:
-        next_x = start_x + command[element][0]
-        next_y = start_y + command[element][1]
+        next_x = start_x + directions[element][0]
+        next_y = start_y + directions[element][1]
         
-        route = (start_x, start_y, next_x, next_y)
+        route = ((start_x, start_y, next_x, next_y))
+        route_reverse = ((next_x, next_y, start_x, start_y))
         
-        if  -5 <= next_x <= 5 and -5 <= next_y <= 5: 
+        if -5 <= next_x <= 5 and -5 <= next_y <= 5:
             visited.add(route)
-            visited.add((next_x, next_y, start_x, start_y)) 
+            visited.add(route_reverse)
+            
             start_x, start_y = next_x, next_y
     
-    answer = len(visited) // 2  
-    
+    answer = len(visited) // 2
     
     return answer
