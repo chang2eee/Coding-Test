a8aa5247a5c8b20aa16d467245f14d02e66c2c98
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-02-29 19:44:12 +0900

    [level 2] Title: 무인도 여행, Time: 0.25 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,39 +1,44 @@
 from collections import deque
-dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
 
-def island(maps, i, j, n, m, visit):
-    visit[i][j] = 1
-    q = deque()
-    q.append([i, j])
+def bfs(maps, visited, y, x):
     days = 0
-    while q:
-        i, j = q.popleft()
-        days += int(maps[i][j])
-        for d in range(4):
-            x, y = i + dx[d], j + dy[d]
-            if not(0 <= x < n and 0 <= y < m): continue
-            if visit[x][y] == 0 and maps[x][y] != 'X':
-                q.append([x, y])
-                visit[x][y] = 1
+    
+    visited[y][x] = True
+    
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
+    queue = deque([(y, x)])
+    
+    while queue:
+        y, x = queue.popleft()
+        days += int(maps[y][x])
+        
+        for i in range(4):
+            next_y = y + dy[i]
+            next_x = x + dx[i]
+            
+            if 0 <= next_y <= len(maps) - 1 and 0 <= next_x <= len(maps[0]) - 1 and visited[next_y][next_x] == False:
+                if maps[next_y][next_x] != 'X':
+                    queue.append((next_y, next_x))
+                    visited[next_y][next_x] = True
+    
     return days
 
 def solution(maps):
     answer = []
     
+    n, m = len(maps), len(maps[0])
+    
+    visited = [[False] * m for _ in range(n)]
+    
     for i in range(len(maps)):
-        maps[i] = list(maps[i])
-        
-    n = len(maps)
-    m = len(maps[0])
+        for j in range(len(maps[0])):
+            if maps[i][j] != 'X' and visited[i][j] == 0:
+                answer.append(bfs(maps, visited, i, j))
     
-    visit = [[0] * m for _ in range(n)]
-    
-    for i in range(n):
-        for j in range(m):
-            if maps[i][j] != 'X' and visit[i][j] == 0:
-                answer.append(island(maps, i, j, n, m, visit))
-                
     if answer:
         return sorted(answer)
     else:
         return [-1]
+    
