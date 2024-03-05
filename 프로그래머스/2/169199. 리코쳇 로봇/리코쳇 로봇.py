c10ac2ca799d780fe84a758eac74f82e378cb856
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-03-05 16:36:31 +0900

    [level 2] Title: 리코쳇 로봇, Time: 1.31 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,50 +1,53 @@
 from collections import deque
 
-def solution(board):
-    answer = 0
-    
+def bfs(board, start, end):
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     
     start_y, start_x = 0, 0
+    end_y, end_x = 0, 0
     
-    # 방문 좌표 기록
-    visited = set()
-    
-    # 로봇 위치 찾기
+    # 시작 / 탈출 좌표 구하기
     for i in range(len(board)):
         for j in range(len(board[0])):
-            if board[i][j] == 'R':
+            if board[i][j] == start:
                 start_y, start_x = i, j
+            if board[i][j] == end:
+                end_y, end_x = i, j
     
-    # y, x, 거리
-    queue = deque([(start_y, start_x, 0)])
+    visited = set()
+    value = 0
+    
+    queue = deque([(start_y, start_x, value)])
     
     while queue:
-        y, x, answer = queue.popleft()
+        y, x, value = queue.popleft()
         
+        if y == end_y and x == end_x:
+            return value
+        
+        # 방문한 좌표 체크
         if (y, x) in visited:
             continue
-        
-        if board[y][x] == 'G':
-            return answer
-    
         visited.add((y, x))
-
+        
         for i in range(4):
-            now_y = y
-            now_x = x
-            
+            now_y, now_x = y, x
             while True:
-                next_x = now_x + dx[i]
                 next_y = now_y + dy[i]
+                next_x = now_x + dx[i]
                 
-                if not (0 <= next_y <= len(board) - 1 and 0 <= next_x <= len(board[0]) -1 and board[next_y][next_x] != 'D'):
+                if (0 <= next_y <= len(board)-1 and 0 <= next_x <= len(board[0])-1 and board[next_y][next_x] != 'D') == False:
                     break
-                    
-                now_x = next_x
-                now_y = next_y
                 
-            queue.append((now_y, now_x, answer + 1))
-    
+                now_y = next_y
+                now_x = next_x
+                
+            queue.append((now_y, now_x, value+1))
+                
     return -1
+            
+def solution(board):
+    answer = bfs(board, 'R', 'G')
+    
+    return answer
