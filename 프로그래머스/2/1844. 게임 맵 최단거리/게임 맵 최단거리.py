62c1693848a28ec95c50112a007ceb77159b6ed6
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-03-08 17:53:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 16.99 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,17 +1,10 @@
 from collections import deque
 
-def bfs(start, end, maps):
+def bfs(maps, start, end, visited):
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     
-    start_y, start_x = 0, 0
-    end_y, end_x = 0, 0
-    
-    n, m = len(maps), len(maps[0])
-    
-    visited = [[False] * m for _ in range(n)]
-    
-    value = 0
+    start_y, start_x, end_y, end_x = 0, 0, 0, 0
     
     for i in range(len(maps)):
         for j in range(len(maps[0])):
@@ -19,38 +12,34 @@ def bfs(start, end, maps):
                 start_y, start_x = i, j
             if maps[i][j] == end:
                 end_y, end_x = i, j
-                
-    queue = deque([(start_y, start_x, value)])
     
+    distance = 0
+    
+    queue = deque([(start_y, start_x, distance)])
+
     while queue:
-        y, x, value = queue.popleft()
+        y, x, distance = queue.popleft()
         
         if y == end_y and x == end_x:
-            return value + 1
+            return distance + 1
         
         for i in range(4):
             next_y = y + dy[i]
             next_x = x + dx[i]
             
-            if 0 <= next_y <= len(maps) - 1 and 0 <= next_x <= len(maps[0]) - 1 and maps[next_y][next_x] != 0:
+            if 0 <= next_y <= len(maps)-1 and 0 <= next_x <= len(maps[0])-1 and maps[next_y][next_x] != 0:
                 if visited[next_y][next_x] == False:
-                    queue.append((next_y, next_x, value + 1))
+                    queue.append((next_y, next_x, distance+1))
                     visited[next_y][next_x] = True
-    
+            
     return -1
     
-    
-    
-    
-    
-    
-    
-
 def solution(maps):
     answer = 0
+    
     maps[0][0] = 'S'
     maps[-1][-1] = 'E'
     
-    answer = bfs('S', 'E', maps)
+    visited = [[False] * len(maps[0]) for _ in range(len(maps))]
     
-    return answer
+    return bfs(maps, 'S', 'E', visited)
