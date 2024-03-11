456df8e7df976727f5fdb7638c330cf35940f8cc
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-03-11 21:21:02 +0900

    [level 2] Title: 무인도 여행, Time: 0.50 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -18,7 +18,7 @@ def bfs(maps, visited, y, x):
             next_y = y + dy[i]
             next_x = x + dx[i]
             
-            if 0 <= next_y <= len(maps) - 1 and 0 <= next_x <= len(maps[0]) - 1 and visited[next_y][next_x] == False:
+            if 0 <= next_y < len(maps) and 0 <= next_x < len(maps[0]) and not visited[next_y][next_x]:
                 if maps[next_y][next_x] != 'X':
                     queue.append((next_y, next_x))
                     visited[next_y][next_x] = True
@@ -28,17 +28,15 @@ def bfs(maps, visited, y, x):
 def solution(maps):
     answer = []
     
-    n, m = len(maps), len(maps[0])
-    
-    visited = [[False] * m for _ in range(n)]
+    visited = [[False] * len(maps[0]) for _ in range(len(maps))]
     
     for i in range(len(maps)):
         for j in range(len(maps[0])):
-            if maps[i][j] != 'X' and visited[i][j] == 0:
+            if maps[i][j] != 'X' and visited[i][j] == False:
+
                 answer.append(bfs(maps, visited, i, j))
     
     if answer:
         return sorted(answer)
     else:
         return [-1]
-    
