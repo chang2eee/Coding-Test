c0eb640c816743c378b030ba423bcf83465e127e
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-03-23 01:56:35 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,12 +1,12 @@
 -- 코드를 작성해주세요
-
-SELECT A.ID AS ID, 
-    B.FISH_NAME AS FISH_NAME, 
+SELECT A.ID AS ID,
+    B.FISH_NAME AS FISH_NAME,
     A.LENGTH AS LENGTH
 FROM FISH_INFO AS A JOIN FISH_NAME_INFO AS B
     ON A.FISH_TYPE = B.FISH_TYPE
-WHERE (A.FISH_TYPE, A.LENGTH) IN (SELECT FISH_TYPE, MAX(LENGTH)
-                                   FROM FISH_INFO
-                                   WHERE LENGTH > 10
-                                   GROUP BY FISH_TYPE)
-ORDER BY ID
+WHERE (A.FISH_TYPE, A.LENGTH) IN (SELECT FISH_TYPE, MAX(LENGTH) 
+                                FROM FISH_INFO 
+                                WHERE LENGTH IS NOT NULL 
+                                GROUP BY FISH_TYPE)
+ORDER BY ID 
+-- 7번 줄 같이 묶어주기 --
