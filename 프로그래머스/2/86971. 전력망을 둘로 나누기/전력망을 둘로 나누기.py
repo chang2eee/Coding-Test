30bdd306a345d0760fdde3ebad2dddd597a23c2f
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-03-23 20:15:27 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.39 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,30 +1,35 @@
+def dfs(node, parent, graph, n, min_diff):
+    count = 1
+    for neighbor in graph[node]:
+        if neighbor != parent:
+            count += dfs(neighbor, node, graph, n, min_diff)
+    diff = abs(n - 2*count)
+    
+    min_diff[0] = min(min_diff[0], diff)
+    return count
+
 def solution(n, wires):
-    # 그래프 생성
-    graph = [[] for _ in range(n + 1)]
-    for v1, v2 in wires:
-        graph[v1].append(v2)
-        graph[v2].append(v1)
-
-    min_diff = float('inf')
-
-    # DFS 함수 정의
-    def dfs(node, parent):
-        nonlocal min_diff
-        count = 1
-        for neighbor in graph[node]:
-            if neighbor != parent:
-                count += dfs(neighbor, node)
-        diff = abs(n - 2 * count)
-        min_diff = min(min_diff, diff)
-        return count
-
-    # 각 전선을 끊어가며 송전탑 개수 차이 계산
+    answer = 0
+    graph = dict()
+    
+    for start, end in wires:
+        if start not in graph:
+            graph[start] = [end]
+        else:
+            graph[start].append(end)
+        if end not in graph:
+            graph[end] = [start]
+        else:
+            graph[end].append(start)
+            
+    min_diff = [float('inf')]
     for v1, v2 in wires:
         graph[v1].remove(v2)
         graph[v2].remove(v1)
-        dfs(v1, v2)
-        dfs(v2, v1)
+        dfs(v1, v2, graph, n, min_diff)
+        dfs(v2, v1, graph, n, min_diff)
         graph[v1].append(v2)
         graph[v2].append(v1)
-
-    return min_diff
+    
+    answer = min_diff[0]
+    return answer
