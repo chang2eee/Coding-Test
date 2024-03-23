7cea5d42d7cf3e0ee64b9367c905736456b583bd
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-03-23 19:10:37 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -4,7 +4,7 @@ def solution(jobs):
     answer = 0
     
     now, count = 0, 0
-    start = -1  # 시작 전 표시
+    start = -1
     
     heap = []
     
@@ -12,20 +12,14 @@ def solution(jobs):
         for job in jobs:
             if start < job[0] <= now:
                 heappush(heap, (job[1], job[0]))
-                
+        
         if len(heap) > 0:
-            execute_time, arrival_time = heappop(heap)
+            run_time, arrival_time = heappop(heap)
             
             start = now
-            now += execute_time
-            
-            answer += now - arrival_time
-            
+            now += run_time
+            answer += (now - arrival_time)
             count += 1
-        
         else:
             now += 1
-            
-    answer //= count
-    
-    return answer
+    return answer // count
