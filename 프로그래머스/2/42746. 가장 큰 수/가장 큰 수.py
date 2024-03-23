8d6c1594bbab9e183b398c5cee8e1f1dfe8b024d
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-03-23 19:36:53 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,13 +1,12 @@
 def solution(numbers):
-    def compare(x, y):
-        return (int(y + x)) - (int(x + y))
-
-    numbers = list(map(str, numbers))
-
-    numbers.sort(key=lambda x: x*10, reverse=True)
-
-    answer = ''.join(numbers)
-
-    answer = str(int(answer))
-
-    return answer
+    answer = ''
+    
+    numbers = [str(number) for number in numbers]
+    numbers.sort(key=lambda x:x*10, reverse=True)
+    
+    for n in numbers:
+        answer += n
+    
+    
+    
+    return str(int(answer))
