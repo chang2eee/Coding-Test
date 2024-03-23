48d3c132b3d4c0bbf5a0440e59d2819b93be1c7a
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-03-23 20:10:30 +0900

    [level 2] Title: 피로도, Time: 0.09 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,25 +1,20 @@
 answer = 0
 
-def dfs(k, dungeons, count, visited):
+def dfs(k, dungeons, visited, count):
     global answer
-    
     answer = max(answer, count)
     
     for i in range(len(dungeons)):
         if visited[i] == False and k >= dungeons[i][0]:
             visited[i] = True
-            dfs(k - dungeons[i][1], dungeons, count + 1, visited)
-        
+            dfs(k-dungeons[i][1], dungeons, visited, count+1)
             visited[i] = False
-    
-    
-    
+
 
 def solution(k, dungeons):
     global answer
     
     visited = [False] * len(dungeons)
-    
-    dfs(k, dungeons, 0, visited)
+    dfs(k, dungeons, visited, 0)
     
     return answer
