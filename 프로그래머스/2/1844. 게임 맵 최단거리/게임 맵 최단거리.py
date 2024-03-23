3fec965c024f5fe57ad8eedd6a01e8d0537d9add
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-03-23 22:29:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.66 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,9 +1,7 @@
 from collections import deque
 
 def bfs(maps, start, end, visited):
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    
+    dy, dx = [0, 0, -1, 1], [-1, 1, 0, 0]
     start_y, start_x, end_y, end_x = 0, 0, 0, 0
     
     for i in range(len(maps)):
@@ -12,16 +10,16 @@ def bfs(maps, start, end, visited):
                 start_y, start_x = i, j
             if maps[i][j] == end:
                 end_y, end_x = i, j
-    
     distance = 0
     
     queue = deque([(start_y, start_x, distance)])
-
+    visited[start_y][start_x] = True
     while queue:
         y, x, distance = queue.popleft()
         
+        
         if y == end_y and x == end_x:
-            return distance + 1
+            return 1+distance
         
         for i in range(4):
             next_y = y + dy[i]
@@ -31,15 +29,13 @@ def bfs(maps, start, end, visited):
                 if visited[next_y][next_x] == False:
                     queue.append((next_y, next_x, distance+1))
                     visited[next_y][next_x] = True
-            
     return -1
-    
+
 def solution(maps):
     answer = 0
     
-    maps[0][0] = 'S'
-    maps[-1][-1] = 'E'
-    
+    maps[0][0], maps[-1][-1] = 'S', 'E'
     visited = [[False] * len(maps[0]) for _ in range(len(maps))]
-    
     return bfs(maps, 'S', 'E', visited)
+    
+    return answer
