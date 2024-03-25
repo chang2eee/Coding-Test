adffbffd1aec82a38eaa34eaffca2add137d0f6a
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-03-26 03:23:26 +0900

    [level 2] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,13 +1,11 @@
 def solution(targets):
-    answer = 0 
-    
-    targets.sort(key = lambda x: [x[1], x[0]])
-    
-    start, end = 0, 0
+    answer = 0
+    targets.sort(key=lambda x:[x[1], x[0]])
+    start = 0
     
     for target in targets:
-        if target[0] >= end:
+        if start <= target[0]:
+            start = target[1]
             answer += 1
-            end = target[1]
-
-    return answer 
+    
+    return answer
