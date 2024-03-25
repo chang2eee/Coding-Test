0d21058a651fecb7d3d73d36fd003061f239f18a
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-03-26 02:37:09 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,7 +1,7 @@
 def solution(n, costs):
     answer = 0
     costs.sort(key = lambda x: x[2])
-    
+    print(costs)
     link = set([costs[0][0]])
 
     while len(link) != n:
@@ -9,7 +9,8 @@ def solution(n, costs):
             if v[0] in link and v[1] in link:
                 continue
             if v[0] in link or v[1] in link:
-                link.update([v[0], v[1]])
+                link.add(v[0])
+                link.add(v[1])
                 answer += v[2]
                 break
                 
