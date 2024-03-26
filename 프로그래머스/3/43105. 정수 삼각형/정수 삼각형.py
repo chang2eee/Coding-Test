5eb135af45ab48087e611fcc6ef520ae73537649
김병창 <106654981+chang2eee@users.noreply.github.com>
2024-03-26 13:23:19 +0900

    [level 3] Title: 정수 삼각형, Time: 54.29 ms, Memory: 14.4 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,16 +1,24 @@
 def solution(triangle):
-    answer = 0
+    # 삼각형의 높이
+    height = len(triangle)
     
-    height = len(triangle) -1
+    # 높이가 1일 때는 그 값이 정답
+    if height == 1:
+        return triangle[0][0]
     
-    while height > 0:
-        for i in range(height):
-            triangle[height-1][i] += max(triangle[height][i], triangle[height][i+1])
-            
-        height -= 1
+    # 삼각형의 아래에서 두 번째 줄부터 시작하여
+    # 각 위치까지의 최대값을 계산하여 저장
+    for i in range(1, height):
+        for j in range(len(triangle[i])):
+            # 맨 왼쪽 값일 때는 위에서 오는 경로가 없음
+            if j == 0:
+                triangle[i][j] += triangle[i - 1][j]
+            # 맨 오른쪽 값일 때는 위에서 오는 경로가 없음
+            elif j == len(triangle[i]) - 1:
+                triangle[i][j] += triangle[i - 1][j - 1]
+            # 그 외의 경우에는 위에서 오는 두 경로 중 큰 값을 선택하여 더함
+            else:
+                triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j])
     
-    answer = triangle[0][0]
-    
-    
-    
-    return answer
+    # 마지막 줄의 값 중 최대값이 최종 결과
+    return max(triangle[-1])
